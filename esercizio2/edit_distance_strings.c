a426fbdabe8244288a2ccf0beb54c45bcb2e2c7c
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-01-03 19:29:20 +0100

    Edit_distance basic version skeleton

--- esercizio2/edit_distance_strings.c
+++ esercizio2/edit_distance_strings.c
@@ -1,7 +1,38 @@
 
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
 
 
-int edit_distance_strings() {
+
+
+
+int min_val (int one_op, int second_op) {
+    return (one_op < second_op) ? one_op : second_op;
+}
+
+int _edit_distance_strings(char *string_one, char *string_two) {
+    int dno_op;
+    int dcanc;
+    int dins;
+
+    if (string_one == NULL)
+        return strlen(string_two);
+    if(string_two == NULL)
+        return strlen(string_one);
+
+    if(string_one[0] == string_two[0])
+        dno_op = _edit_distance_strings(string_one+1, string_two+1);
+
+    dcanc = 1 + _edit_distance_strings(string_one, string_two+1);
+
+    dins = 1 + _edit_distance_strings(string_one+1, string_two);
+
+    return min_val(dno_op, min_val(dcanc, dins));
 
 }
+
+
+
+
 
