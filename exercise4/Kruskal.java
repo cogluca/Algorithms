bc58165fcf103bfcb9c3505ead0353a7ec97a3c3
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-01-13 18:42:59 +0100

    strange import error fixed + indentation

--- exercise4/Kruskal.java
+++ exercise4/Kruskal.java
@@ -1,59 +1,59 @@
-package exercise4;
+    package exercise4;
 
-import exercise3.UnionFindSet;
-import exercise4.datastructure.Compare;
-import exercise4.datastructure.Edge;
-import exercise4.datastructure.Graph;
-import exercise4.datastructure.Node;
+    import exercise3.UnionFindSet;
+    import exercise4.datastructure.Compare;
+    import exercise4.datastructure.Edge;
+    import exercise4.datastructure.Graph;
+    import exercise4.datastructure.Node;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
-
-public class Kruskal {
+    import java.util.ArrayList;
+    import java.util.Collections;
 
 
-    public static Graph<String, Float> tryKruskal(Graph<String,Float> graphToScan) throws Exception {
-
-        Graph<String, Float> graph = new Graph<>(false);
-        UnionFindSet<Node<String, Float>> unionFindSet = new UnionFindSet<>();
-        ArrayList<Edge<String, Float>> listOfEdges = new ArrayList<>();
+    public class Kruskal {
 
 
-        listOfEdges.addAll(((ArrayList<Edge<String,Float>>)graphToScan.getEdges()));
+        public static Graph<String, Float> tryKruskal(Graph<String,Float> graphToScan) throws Exception {
 
-        for (Node<String, Float> iteratedNode : graphToScan.getNodes()) {
-            unionFindSet.makeSet(iteratedNode);
-        }
-
-        Collections.sort(listOfEdges, new Compare());
+            Graph<String, Float> graph = new Graph<>(false);
+            UnionFindSet<Node<String, Float>> unionFindSet = new UnionFindSet<>();
+            ArrayList<Edge<String, Float>> listOfEdges = new ArrayList<>();
 
 
+            listOfEdges.addAll(((ArrayList<Edge<String,Float>>)graphToScan.getEdges()));
 
-        for (Edge<String, Float> edge : listOfEdges) {
-            // System.out.println(" " + arc.getNodeFrom().getValue() + " - " +
-            // arc.getNodeTo().getValue());
-            if (unionFindSet.findSet(edge.getFirstNode()) != unionFindSet.findSet(edge.getSecondNode())) {
-                if (!graph.containsNode(edge.getFirstNode().getValue())) {
-                    graph.addNode(edge.getFirstNode().getValue());
-                }
-                if (!graph.containsNode(edge.getSecondNode().getValue())) {
-                    graph.addNode(edge.getSecondNode().getValue());
-                }
-                unionFindSet.unionSet(edge.getFirstNode(), edge.getSecondNode());
-                if (!graph.containsEdge(edge.getFirstNode().getValue(), edge.getSecondNode().getValue())) {
-                    graph.addEdge(edge.getFirstNode().getValue(), edge.getSecondNode().getValue(), edge.getLabel());
-                }
+            for (Node<String, Float> iteratedNode : graphToScan.getNodes()) {
+                unionFindSet.makeSet(iteratedNode);
             }
 
+            Collections.sort(listOfEdges, new Compare());
+
+
+
+            for (Edge<String, Float> edge : listOfEdges) {
+                // System.out.println(" " + arc.getNodeFrom().getValue() + " - " +
+                // arc.getNodeTo().getValue());
+                if (unionFindSet.findSet(edge.getFirstNode()) != unionFindSet.findSet(edge.getSecondNode())) {
+                    if (!graph.containsNode(edge.getFirstNode().getValue())) {
+                        graph.addNode(edge.getFirstNode().getValue());
+                    }
+                    if (!graph.containsNode(edge.getSecondNode().getValue())) {
+                        graph.addNode(edge.getSecondNode().getValue());
+                    }
+                    unionFindSet.unionSet(edge.getFirstNode(), edge.getSecondNode());
+                    if (!graph.containsEdge(edge.getFirstNode().getValue(), edge.getSecondNode().getValue())) {
+                        graph.addEdge(edge.getFirstNode().getValue(), edge.getSecondNode().getValue(), edge.getLabel());
+                    }
+                }
+
+
+            }
+            return graph;
 
         }
-        return graph;
+
+
+
+
 
     }
-
-
-
-
-
-}
--- exercise4/RunKruskal.java
+++ exercise4/RunKruskal.java
@@ -1,42 +1,42 @@
-package exercise4;
+    package exercise4;
 
-import exercise4.datastructure.Graph;
-import exercise4.utils.DataUtils;
+    import exercise4.datastructure.Graph;
+    import exercise4.utils.DataUtils;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class RunKruskal {
+    public class RunKruskal {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Graph<String, Float> toTestGraph;
-        Graph<String, Float> returnedForest;
-        float distance = 0;
+            Graph<String, Float> toTestGraph;
+            Graph<String, Float> returnedForest;
+            float distance = 0;
 
-        String filename;
-        Scanner scanner = new Scanner(System.in);
+            String filename;
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Would like to know which file you're trying to load");
-        filename = scanner.nextLine();
+            System.out.println("Would like to know which file you're trying to load");
+            filename = scanner.nextLine();
 
-        try {
-            toTestGraph = DataUtils.loadGraph(DataUtils.loadData(filename));
-            returnedForest = Kruskal.tryKruskal(toTestGraph);
-            distance = DataUtils.getTotalForestDistance(toTestGraph);
+            try {
+                toTestGraph = DataUtils.loadGraph(DataUtils.loadData(filename));
+                returnedForest = Kruskal.tryKruskal(toTestGraph);
+                distance = DataUtils.getTotalForestDistance(toTestGraph);
 
-            System.out.println("Total distance in this minimal forest is: " + distance/1000);
-            System.out.println("Total number of edges in this forest is: " + toTestGraph.graphEdgeSize());
-            System.out.println("Total number of nodes in this forest is: " + returnedForest.graphNodeSize());
+                System.out.println("Total distance in this minimal forest is: " + distance/1000);
+                System.out.println("Total number of edges in this forest is: " + toTestGraph.graphEdgeSize());
+                System.out.println("Total number of nodes in this forest is: " + returnedForest.graphNodeSize());
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-            System.out.println("Something went wrong during file loading and unpacking");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("Something went wrong during file loading and unpacking");
+            }
+
+
         }
 
-
     }
-
-}
 
 
 
