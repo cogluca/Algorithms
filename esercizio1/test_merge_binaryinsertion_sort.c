fe06d033778e1e82deda5f3ba6083ca45987ef17
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-01-20 06:11:55 +0100

    minor fixes and typos across exercises

--- esercizio1/test_merge_binaryinsertion_sort.c
+++ esercizio1/test_merge_binaryinsertion_sort.c
@@ -35,30 +35,6 @@
         int *correct_array[] = {&arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5], &arr[6]};
 
 
-
-        for (int i = 0; i <= 8; i++) {
-
-            printf("for k = %d\n", i);
-
-            for(int ii = 0; ii < 7; ii++){
-                array[ii] = array2[ii];
-                printf("%d ", *array[ii]);
-                printf("\n");
-            }
-            printf("\n");
-
-            merge_binary_insertion_sort_entry((void **) array, 7, i, comp_int);
-
-            for(int ii = 0; ii < 7; ii++){
-                printf("%d ", *array[ii]);
-                printf("\n");
-            }
-            printf("\n");
-
-
-
-        }
-
         TEST_ASSERT_EQUAL_PTR_ARRAY((void **) correct_array, (void **) array, 7);
 
     }
